0c3fb53ddfecb4e69d1dc5f4b4809416ef367877
Shamsul Khan <imrank3117@gmail.com>
2023-03-07 16:25:19 -0600

    challenge

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_07_172126) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_07_175838) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
+    t.boolean "completed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
